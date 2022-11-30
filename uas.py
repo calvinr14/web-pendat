0f9f1c81ef5e870f24f2ac71d69036d0eaafde7e
calvinr14 <112038885+calvinr14@users.noreply.github.com>
2022-11-30 19:21:33 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -1,177 +1,174 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn import preprocessing
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from numpy import array
-from sklearn import tree
-from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
-from sklearn.tree import DecisionTreeClassifier
-from collections import OrderedDict
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.datasets import make_classification
-from sklearn.svm import SVC
-import altair as alt
-from sklearn.utils.validation import joblib
-
-st.title("PENAMBANGAN DATA")
-st.write("##### Nama  : Calvin Rifansyah")
-st.write("##### Nim   : 200411100072 ")
-st.write("##### Kelas : Penambangan Data C ")
-data_set_description, upload_data, preporcessing, modeling, implementation = st.tabs(["Data Set Description", "Upload Data", "Prepocessing", "Modeling", "Implementation"])
-
-with data_set_description:
-    st.write("""# Data Set Description """)
-    st.write("###### Data Set Ini Adalah : Brain Tumor (Tumor Otak) ")
-    st.write("###### Sumber Data Set dari Kaggle : https://www.kaggle.com/datasets/jillanisofttech/brain-tumor")
-    
-with upload_data:
-    st.write("""# Upload File""")
-    uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        df = pd.read_csv(uploaded_file)
-        st.write("Nama File Anda = ", uploaded_file.name)
-        st.dataframe(df)
-
-with preporcessing:
-    st.write("""# Preprocessing""")
-    df[["X53416", "M83670", "X90908", "M97496"]].agg(['min','max'])
-
-    df.y.value_counts()
-    df = df.drop(columns=["Unnamed: 0"])
-
-    X = df.drop(columns="y")
-    Y = df.y
-    "### Membuang fitur yang tidak diperlukan"
-    df
-    X
-
-    le = preprocessing.LabelEncoder()
-    le.fit(Y)
-    Y = le.transform(Y)
-
-    "### Transformasi Label"
-    Y
-
-    le.inverse_transform(Y)
-
-    labels = pd.get_dummies(df.y).columns.values.tolist()
-
-    "### Label"
-    labels
-
-    scaler = MinMaxScaler()
-    scaler.fit(X)
-    X = scaler.transform(X)
-    "### Normalize data transformasi"
-    X
-
-    X.shape, Y.shape
-
-    le.inverse_transform(Y)
-
-    labels = pd.get_dummies(df.y).columns.values.tolist()
-    
-
-with modeling:
-    X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=4)
+    import streamlit as st
+    import pandas as pd
+    import numpy as np
+    from sklearn import preprocessing
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.model_selection import train_test_split
+    from sklearn.neighbors import KNeighborsClassifier
+    from numpy import array
+    from sklearn import tree
+    from sklearn.naive_bayes import GaussianNB
     from sklearn.preprocessing import StandardScaler
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-    st.write("""# Modeling """)
-    st.subheader("Berikut ini adalah pilihan untuk Modeling")
-    st.write("Pilih Model yang Anda inginkan untuk Cek Akurasi")
-    naive = st.checkbox('Naive Bayes')
-    kn = st.checkbox('K-Nearest Neighbor')
-    des = st.checkbox('Decision Tree')
-    mod = st.button("Modeling")
+    from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
+    from sklearn.tree import DecisionTreeClassifier
+    from collections import OrderedDict
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.ensemble import BaggingClassifier
+    from sklearn.datasets import make_classification
+    from sklearn.svm import SVC
+    import altair as alt
+    from sklearn.utils.validation import joblib
 
-    # NB
-    GaussianNB(priors=None)
+    st.title("PENAMBANGAN DATA")
+    st.write("##### Nama  : Calvin Rifansyah")
+    st.write("##### Nim   : 200411100072 ")
+    st.write("##### Kelas : Penambangan Data C ")
+    data_set_description, upload_data, preporcessing, modeling, implementation = st.tabs(["Data Set Description", "Upload Data", "Prepocessing", "Modeling", "Implementation"])
 
-    # Fitting Naive Bayes Classification to the Training set with linear kernel
-    nvklasifikasi = GaussianNB()
-    nvklasifikasi = nvklasifikasi.fit(X_train, Y_train)
+    with data_set_description:
+        st.write("""# Data Set Description """)
+        st.write("###### Data Set Ini Adalah : Brain Tumor (Tumor Otak) ")
+        st.write("###### Sumber Data Set dari Kaggle : https://www.kaggle.com/datasets/jillanisofttech/brain-tumor")
+        
+    with upload_data:
+        st.write("""# Upload File""")
+        uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
+        for uploaded_file in uploaded_files:
+            df = pd.read_csv(uploaded_file)
+            st.write("Nama File Anda = ", uploaded_file.name)
+            st.dataframe(df)
 
-    # Predicting the Test set results
-    Y_pred = nvklasifikasi.predict(X_test)
-    
-    Y_compare = np.vstack((Y_test,Y_pred)).T
-    nvklasifikasi.predict_proba(X_test)
-    akurasi = round(100 * accuracy_score(Y_test, Y_pred))
-    # akurasi = 10
+    with preporcessing:
+        st.write("""# Preprocessing""")
+        df[["X53416", "M83670", "X90908", "M97496"]].agg(['min','max'])
 
-    # KNN 
-    K=10
-    knn=KNeighborsClassifier(n_neighbors=K)
-    knn.fit(X_train,Y_train)
-    Y_pred=knn.predict(X_test)
+        df.y.value_counts()
+        df = df.drop(columns=["Unnamed: 0"])
 
-    skor_akurasi = round(100 * accuracy_score(Y_test,Y_pred))
+        X = df.drop(columns="y")
+        Y = df.y
+        "### Membuang fitur yang tidak diperlukan"
+        df
+        X
 
-    # DT
+        le = preprocessing.LabelEncoder()
+        le.fit(Y)
+        Y = le.transform(Y)
 
-    dt = DecisionTreeClassifier()
-    dt.fit(X_train, Y_train)
-    # prediction
-    dt.score(X_test, Y_test)
-    Y_pred = dt.predict(X_test)
-    #Accuracy
-    akurasiii = round(100 * accuracy_score(Y_test,Y_pred))
+        "### Transformasi Label"
+        Y
 
-    if naive :
-        if mod :
-            st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(akurasi))
-    if kn :
-        if mod:
-            st.write("Model KNN accuracy score : {0:0.2f}" . format(skor_akurasi))
-    if des :
-        if mod :
-            st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(akurasiii))
-    
-    eval = st.button("Evaluasi semua model")
-    if eval :
-        # st.snow()
-        source = pd.DataFrame({
-            'Nilai Akurasi' : [akurasi,skor_akurasi,akurasiii],
-            'Nama Model' : ['Naive Bayes','KNN','Decision Tree']
-        })
+        le.inverse_transform(Y)
 
-        bar_chart = alt.Chart(source).mark_bar().encode(
-            Y = 'Nilai Akurasi',
-            x = 'Nama Model'
-        )
+        labels = pd.get_dummies(df.y).columns.values.tolist()
 
-        st.altair_chart(bar_chart,use_container_width=True) 
+        "### Label"
+        labels
 
-with implementation:
-    st.write("# Implementation")
-    Precipitation = st.number_input('Masukkan preciptation (curah hujan) : ')
-    Temp_Max = st.number_input('Masukkan tempmax (suhu maks) : ')
-    Temp_Min = st.number_input('Masukkan tempmin (suhu min) : ')
-    Wind = st.number_input('Masukkan wind (angin) : ')
+        scaler = MinMaxScaler()
+        scaler.fit(X)
+        X = scaler.transform(X)
+        "### Normalize data transformasi"
+        X
 
-    def submit():
-        # input
-        inputs = np.array([[
-            Precipitation,
-            Temp_Max,
-            Temp_Min,
-            Wind
-            ]])
-        le = joblib.load("le.save")
-        model1 = joblib.load("knn.joblib")
-        y_pred3 = model1.predict(inputs)
-        st.write(f"Berdasarkan data yang di masukkan, maka anda prediksi cuaca : {le.inverse_transform(y_pred3)[0]}")
+        X.shape, Y.shape
 
-    all = st.button("Submit")
-    if all :
-        st.balloons()
-        submit()
+        le.inverse_transform(Y)
+
+        labels = pd.get_dummies(df.y).columns.values.tolist()
+        
+
+    with modeling:
+        X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=4)
+        from sklearn.linear_model import LogisticRegression
+        log_reg = LogisticRegression()
+        log_reg.fit(X_train,Y_train)
+        LogisticRegression()
+        st.write("""# Modeling """)
+        st.subheader("Berikut ini adalah pilihan untuk Modeling")
+        st.write("Pilih Model yang Anda inginkan untuk Cek Akurasi")
+        naive = st.checkbox('Logistic Regression')
+        kn = st.checkbox('K-Nearest Neighbor')
+        des = st.checkbox('Decision Tree')
+        mod = st.button("Modeling")
+
+        # Logistic Regression
+
+        # Predicting the Test set results
+        Y_predict = log_reg.predict(X_test)
+        Y_predict
+
+        ## Accuracy
+        score = log_reg.score(X_test, Y_test)
+        print(score)
+
+        # akurasi = 10
+
+        # KNN 
+        K=10
+        knn=KNeighborsClassifier(n_neighbors=K)
+        knn.fit(X_train,Y_train)
+        Y_pred=knn.predict(X_test)
+
+        skor_akurasi = round(100 * accuracy_score(Y_test,Y_pred))
+
+        # DT
+
+        dt = DecisionTreeClassifier()
+        dt.fit(X_train, Y_train)
+        # prediction
+        dt.score(X_test, Y_test)
+        Y_pred = dt.predict(X_test)
+        #Accuracy
+        akurasiii = round(100 * accuracy_score(Y_test,Y_pred))
+
+        if naive :
+            if mod :
+                st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(score))
+        if kn :
+            if mod:
+                st.write("Model KNN accuracy score : {0:0.2f}" . format(skor_akurasi))
+        if des :
+            if mod :
+                st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(akurasiii))
+        
+        eval = st.button("Evaluasi semua model")
+        if eval :
+            # st.snow()
+            source = pd.DataFrame({
+                'Nilai Akurasi' : [akurasi,skor_akurasi,akurasiii],
+                'Nama Model' : ['Naive Bayes','KNN','Decision Tree']
+            })
+
+            bar_chart = alt.Chart(source).mark_bar().encode(
+                Y = 'Nilai Akurasi',
+                x = 'Nama Model'
+            )
+
+            st.altair_chart(bar_chart,use_container_width = True) 
+
+    with implementation:
+        st.write("# Implementation")
+        Precipitation = st.number_input('Masukkan preciptation (curah hujan) : ')
+        Temp_Max = st.number_input('Masukkan tempmax (suhu maks) : ')
+        Temp_Min = st.number_input('Masukkan tempmin (suhu min) : ')
+        Wind = st.number_input('Masukkan wind (angin) : ')
+
+        def submit():
+            # input
+            inputs = np.array([[
+                Precipitation,
+                Temp_Max,
+                Temp_Min,
+                Wind
+                ]])
+            le = joblib.load("le.save")
+            model1 = joblib.load("knn.joblib")
+            y_pred3 = model1.predict(inputs)
+            st.write(f"Berdasarkan data yang di masukkan, maka anda prediksi cuaca : {le.inverse_transform(y_pred3)[0]}")
+
+        all = st.button("Submit")
+        if all :
+            st.balloons()
+            submit()
 
